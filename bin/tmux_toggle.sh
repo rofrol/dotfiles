ca5c89e56bc981f386f1d11ece6790143e59350a
Roman Frołow <rofrol@gmail.com>
2019-06-04 12:47:53 +0200

    half_the_width

--- bin/tmux_toggle.sh
+++ bin/tmux_toggle.sh
@@ -7,17 +7,19 @@
 # https://stackoverflow.com/questions/35016458/how-to-write-if-statement-in-tmux-conf-to-set-different-options-for-different-t/36953319#36953319
 # https://devel.tech/tips/n/tMuXrSz9/resize-tmux-main-panes-by-percentage/
 
+half_the_width=170
+
 highest=$(tmux display-message -p '#{window_panes}')
 highest_left=$(tmux display -p -t :1.${highest} '#{pane_left}')
 highest_width=$(tmux display -p -t :1.${highest} '#{pane_width}')
 previous_width=$(tmux display -p -t :1.$((highest-1)) '#{pane_width}')
 both_width=$((highest_width + previous_width))
 
-if [[ $highest_left -gt 0 && $both_width -lt 160 ]] ; then
+if [[ $highest_left -gt 0 && $both_width -lt $half_the_width ]] ; then
     tmux move-pane -v -s ${highest} -t $((highest-1))
     tmux resize-pane -t $((highest-2)) -U 10
     tmux resize-pane -t ${highest} -y 3
-elif [[ $highest_left -eq 0 && $previous_width -gt 160 ]] ; then
+elif [[ $highest_left -eq 0 && $previous_width -gt $half_the_width ]] ; then
     tmux move-pane -h -s ${highest} -t $((highest-1))
     tmux resize-pane -t ${highest} -y 10
 fi

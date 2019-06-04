b84b202e615a4a372b9a8bbe5307613c5a3d9b62
Roman Frołow <rofrol@gmail.com>
2019-06-04 11:13:13 +0200

    Use highest pane number

--- bin/tmux_toggle.sh
+++ bin/tmux_toggle.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-if [[ $(tmux display -p -t :1.3 '#{pane_left}') -eq 0 ]] ; then
-    tmux move-pane -h -s 3 -t 2;
+# https://unix.stackexchange.com/questions/186390/tmux-get-number-of-panes-in-the-current-window-in-bash-variable
+highest_pane_number=$(tmux display-message -p '#{window_panes}')
+previous_pane_number=$((highest_pane_number-1))
+if [[ $(tmux display -p -t :1.${highest_pane_number} '#{pane_left}') -eq 0 ]] ; then
+    tmux move-pane -h -s ${highest_pane_number} -t ${previous_pane_number};
 else
-    tmux move-pane -v -s 3 -t 2;
+    tmux move-pane -v -s ${highest_pane_number} -t ${previous_pane_number};
 fi

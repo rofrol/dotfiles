e2ed77b3e590a5aaae363d296d15b8ae1a0709bf
Roman Frołow <rofrol@gmail.com>
2019-06-04 11:36:24 +0200

    Better resize

--- bin/tmux_toggle.sh
+++ bin/tmux_toggle.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # https://unix.stackexchange.com/questions/186390/tmux-get-number-of-panes-in-the-current-window-in-bash-variable
-highest_pane_number=$(tmux display-message -p '#{window_panes}')
-previous_pane_number=$((highest_pane_number-1))
-if [[ $(tmux display -p -t :1.${highest_pane_number} '#{pane_left}') -eq 0 ]] ; then
-    tmux move-pane -h -s ${highest_pane_number} -t ${previous_pane_number};
+highest=$(tmux display-message -p '#{window_panes}')
+if [[ $(tmux display -p -t :1.${highest} '#{pane_left}') -eq 0 ]] ; then
+    tmux move-pane -h -s ${highest} -t $((highest-1))
+    tmux resize-pane -t ${highest} -y 10
 else
-    tmux move-pane -v -s ${highest_pane_number} -t ${previous_pane_number};
+    tmux move-pane -v -s ${highest} -t $((highest-1))
+    tmux resize-pane -t $((highest-2)) -U 10
+    tmux resize-pane -t ${highest} -y 3
 fi

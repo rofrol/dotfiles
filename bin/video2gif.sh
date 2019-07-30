c0cc06799a530f88f3c3ac690deb6caf3843d72b
Roman Frołow <rofrol@gmail.com>
2019-07-30 07:34:31 +0200

    video2gif.sh - note about optimizing size

--- bin/video2gif.sh
+++ bin/video2gif.sh
@@ -16,3 +16,12 @@ ffmpeg -i  "$vid"                                  \
        -lavfi "$filters [x]; [x][1:v] paletteuse"  \
        -y  "$filename".gif                              &&
 rm palette.png
+
+# Weirdly, generating a palette tripled the size of the GIF, although it greatly increased the quality over the default palette. I found the best way is to generate the GIF with ffmpeg as per usual (possibly with a better palette as in this answer) then just run it through an optimization tool (there's ones online too) that can make unchanging parts of the frame transparent or apply other optimizations. https://superuser.com/questions/1049606/reduce-generated-gif-size-using-ffmpeg#comment1636162_1049820
+
+# I tried `gifsicle -0 -03 input.gif -o output.gif` but that shrink from 14M to 13M.
+# ezgif.com shrinked to 9M, but probably scaled to smaller image.
+# Probably I could use smaller resolution with `ffmped -s 800x600`.
+
+# to convert webm to mp4 `ffmpeg -i input.webm -qscale 0 output.mp4`.
+# https://stackoverflow.com/questions/12659383/ffmpeg-convert-webm-to-mp4-not-working/12659449#12659449

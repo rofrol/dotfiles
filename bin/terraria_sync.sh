d652acf0e31ae1f42b5eee559df97e66feb2d7c4
Roman Frołow <rofrol@gmail.com>
2020-01-20 17:57:29 +0100

    bin/terraria_sync.sh

---
+++ bin/terraria_sync.sh
@@ -0,0 +1,6 @@
+#Because I had problem with space in directory name, I have done this:
+#on OS X: mv ~/Library/Application\ Support/Terraria ~/Terraria
+#rsync [OPTION...] SRC... [USER@]HOST:DEST
+#rsync -azvvcW -e ssh romanfroow@192.168.0.18:/Users/romanfroow/Terraria /media/c/Users/roman/Documents/My\ Games/
+HOST="$1"
+rsync -azvvcW -e ssh /home/roman/.local/share/Terraria/Worlds romanfroow@${HOST}:/Users/romanfroow/Terraria/

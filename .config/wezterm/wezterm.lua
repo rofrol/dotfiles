5822abde29af28bfdac1a889720b3559fc804045
Roman Frołow <rofrol@gmail.com>
2020-08-29 11:15:20 +0200

    wezterm: tabbar styling

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -28,5 +28,57 @@ return {
       bold = false,
       font = wezterm.font("Ubuntu Mono"),
     },
+  },
+  colors = {
+    tab_bar = {
+
+      -- The color of the strip that goes along the top of the window
+      background = "#ffffff",
+
+      -- The active tab is the one that has focus in the window
+      active_tab = {
+        -- The color of the background area for the tab
+        bg_color = "#eeeeee",
+        -- The color of the text for the tab
+        fg_color = "#000000",
+
+        -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+        -- label shown for this tab.
+        -- The default is "Normal"
+        intensity = "Normal",
+
+        -- Specify whether you want "None", "Single" or "Double" underline for
+        -- label shown for this tab.
+        -- The default is "None"
+        underline = "None",
+
+        -- Specify whether you want the text to be italic (true) or not (false)
+        -- for this tab.  The default is false.
+        italic = false,
+
+        -- Specify whether you want the text to be rendered with strikethrough (true)
+        -- or not for this tab.  The default is false.
+        strikethrough = false,
+      },
+
+      -- Inactive tabs are the tabs that do not have focus
+      inactive_tab = {
+        bg_color = "#aaaaaa",
+        fg_color = "#444444",
+
+        -- The same options that were listed under the `active_tab` section above
+        -- can also be used for `inactive_tab`.
+      },
+
+      -- You can configure some alternate styling when the mouse pointer
+      -- moves over inactive tabs
+      inactive_tab_hover = {
+        bg_color = "#ffffff",
+        fg_color = "#000000",
+
+        -- The same options that were listed under the `active_tab` section above
+        -- can also be used for `inactive_tab_hover`.
+      }
+    }
   }
 }

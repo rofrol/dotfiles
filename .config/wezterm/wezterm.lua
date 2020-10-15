d6927cccbcd2a0e8e54550016149806a4df539ae
Roman Frołow <rofrol@gmail.com>
2020-10-15 10:45:58 +0200

    wezterm: git bash from scoop is default. add menu to choose shell.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,32 @@
 local wezterm = require 'wezterm';
 
 return {
-  default_prog = {"bash", "-l"},
+  --default_prog = {"bash", "-l"},
+  default_prog = {"C:/Users/roman.frolow/AppData/Local/Programs/Git/bin/bash", "-l"},
+  launch_menu = {
+    {
+      args = {"top"},
+    },
+    {
+      -- Optional label to show in the launcher. If omitted, a label
+      -- is derived from the `args`
+      label = "Bash",
+      -- The argument array to spawn.  If omitted the default program
+      -- will be used as described in the documentation above
+      args = {"bash", "-l"},
+
+      -- You can specify an alternative current working directory;
+      -- if you don't specify one then a default based on the OSC 7
+      -- escape sequence will be used (see the Shell Integration
+      -- docs), falling back to the home directory.
+      -- cwd = "/some/path"
+
+      -- You can override environment variables just for this command
+      -- by setting this here.  It has the same semantics as the main
+      -- set_environment_variables configuration option described above
+      -- set_environment_variables = { FOO = "bar" },
+    },
+  },
   --only with this light theme I can see command line in nvim
   color_scheme = "Tango Adapted",
   --dpi = 96.0,

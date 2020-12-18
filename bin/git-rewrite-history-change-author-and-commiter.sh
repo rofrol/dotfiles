e19796656ecfbbd4b96c62037e1af9f7e1877ef9
Roman Frołow <rofrol@gmail.com>
2020-12-18 12:03:44 +0100

    git: bin/git-rewrite-history-change-author-and-commiter.sh

---
+++ bin/git-rewrite-history-change-author-and-commiter.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# https://stackoverflow.com/questions/750172/how-to-change-the-author-and-committer-name-and-e-mail-of-multiple-commits-in-gi/750182#750182
+
+git filter-branch --env-filter '
+OLD_EMAIL="your-old-email@example.com"
+CORRECT_NAME="Your Correct Name"
+CORRECT_EMAIL="your-correct-email@example.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
+# to get all authors and commiters
+# https://stackoverflow.com/questions/9597410/list-all-developers-on-a-project-in-git/36325643#36325643
+git log --pretty="%an %ae%n%cn %ce" | sort | uniq
+

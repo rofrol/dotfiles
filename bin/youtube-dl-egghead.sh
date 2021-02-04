5ef6f0bf2037afe610074416074ba790959f1f67
Roman Frołow <rofrol@gmail.com>
2021-02-04 02:41:13 +0100

    youtube-dl-egghead: shorter code to get links

--- bin/youtube-dl-egghead.sh
+++ bin/youtube-dl-egghead.sh
@@ -3,8 +3,9 @@
 # - main thread about downloading egghead videos https://gist.github.com/ldong/b289d56090f98d02423c#gistcomment-3207051
 # - add number to the file with `autonumber` https://gist.github.com/ldong/b289d56090f98d02423c#gistcomment-2275517
 # - copy list of files:
-#   - `copy([...new Set(Array.from(document.querySelectorAll('[href^="/lessons/"]')).map(a => a.href))].join('\n'))`
+#   - `copy([...document.querySelectorAll('[href^="/lessons/"]')].map(a => a.href).join('\n'))`
 #   - https://gist.github.com/ldong/b289d56090f98d02423c#gistcomment-2877198
+#   - https://stackoverflow.com/questions/53350019/how-to-use-map-in-nodelist-in-javascript/53350150#53350150
 # - copy ahoy_visistor cookie
 #   - `document.cookie.split('; ').find(row => row.startsWith('ahoy_visitor=')).split('=')[1]`
 #   - https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie#example_2_get_a_sample_cookie_named_test2

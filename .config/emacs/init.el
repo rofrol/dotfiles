7bc26092294cf5ae3bbdb100cf37381debc7afa9
Roman Frołow <rofrol@gmail.com>
2021-08-06 22:15:38 +0200

    emacs: move-lines-up move-lines-down

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -69,7 +69,7 @@
     (indent-according-to-mode)))
 
 (global-set-key (kbd "M-K") 'delete-line-backwards)
-
+		     
 (defun mark-whole-line ()
   (beginning-of-line)
   (set-mark-command nil)
@@ -86,3 +86,46 @@
       )))
 
 (define-key global-map (kbd "M-w") #'kill-ring-save-whole-line-or-region)
+
+;; https://stackoverflow.com/questions/2423834/move-line-region-up-and-down-in-emacs/12512671#12512671
+;; move the line(s) spanned by the active region up/down (line transposing)
+;; {{{
+(defun move-lines (n)
+  (let ((beg) (end) (keep))
+    (if mark-active 
+        (save-excursion
+          (setq keep t)
+          (setq beg (region-beginning)
+                end (region-end))
+          (goto-char beg)
+          (setq beg (line-beginning-position))
+          (goto-char end)
+          (setq end (line-beginning-position 2)))
+      (setq beg (line-beginning-position)
+            end (line-beginning-position 2)))
+    (let ((offset (if (and (mark t) 
+                           (and (>= (mark t) beg)
+                                (< (mark t) end)))
+                      (- (point) (mark t))))
+          (rewind (- end (point))))
+      (goto-char (if (< n 0) beg end))
+      (forward-line n)
+      (insert (delete-and-extract-region beg end))
+      (backward-char rewind)
+      (if offset (set-mark (- (point) offset))))
+    (if keep
+        (setq mark-active t
+              deactivate-mark nil))))
+
+(defun move-lines-up (n)
+  "move the line(s) spanned by the active region up by N lines."
+  (interactive "*p")
+  (move-lines (- (or n 1))))
+
+(defun move-lines-down (n)
+  "move the line(s) spanned by the active region down by N lines."
+  (interactive "*p")
+  (move-lines (or n 1)))
+
+(global-set-key [(meta up)] 'move-lines-up)
+(global-set-key [(meta down)] 'move-lines-down)

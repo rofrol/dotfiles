9637e3525a1c6bf482abdb4a16c6804f7a427819
Roman Frołow <rofrol@gmail.com>
2021-08-10 19:57:49 +0200

    emacs: typescript with tide and company

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -309,3 +309,23 @@ there's a region, all lines that region covers will be duplicated."
 ;; (setq-default cursor-type 'bar)
 (setq-default cursor-type '(bar . 2))
 ;; (setq-default cursor-type 'hollow)
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)

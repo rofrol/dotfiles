5e76b643ed126a94daf5b6e461e5d4f93da23441
Roman Frołow <rofrol@gmail.com>
2021-09-03 22:29:28 +0200

    mpv: use MPV_HOME in mpv-install.bat

--- bin/mpv-setup.bat
+++ bin/mpv-setup.bat
@@ -1,5 +1,3 @@
-rd /S /Q %USERPROFILE%\scoop\apps\mpv\current\portable_config
-mklink /D %USERPROFILE%\scoop\apps\mpv\current\portable_config %USERPROFILE%\.config\mpv
 cd %USERPROFILE%\personal_projects\
 git clone https://github.com/open-dynaMIX/simple-mpv-webui
 mklink /D %USERPROFILE%\.config\mpv\scripts\webui-page %USERPROFILE%\personal_projects\vendor\simple-mpv-webui\webui-page

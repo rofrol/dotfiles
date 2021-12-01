21faa6ff69e12c962029205a9005bb3a5ce0dc64
Roman Frołow <rofrol@gmail.com>
2021-12-01 14:42:54 +0100

    bin/nixos-available-updates.sh: do not remove result, better output

--- bin/nixos-available-updates.sh
+++ bin/nixos-available-updates.sh
@@ -2,8 +2,7 @@
 
 sudo nix-channel --update
 sudo nixos-rebuild build
-sudo ls -l result
-sudo nix store diff-closures /run/current-system ./result
-sudo rm ./result
+#sudo ls -l result
+sudo nix store diff-closures /run/current-system ./result | awk '/[0-9] →|→ [0-9]/ && !/nixos/' || echo
 
 # https://discourse.nixos.org/t/how-to-get-this-pending-updates-notification-in-gnome/16344

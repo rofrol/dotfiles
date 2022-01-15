e9ae9a67ac491879c5315480229925396cdb7603
Roman Frołow <rofrol@gmail.com>
2022-01-15 20:56:13 +0100

    bin/nixos-available-updates.sh

--- bin/nixos-available-updates.sh
+++ bin/nixos-available-updates.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-sudo nix-channel --update
-sudo nixos-rebuild build
-#sudo ls -l result
-sudo nix store diff-closures /run/current-system ./result | awk '/[0-9] →|→ [0-9]/ && !/nixos/' || echo
+sudo nix-channel --update && \
+sudo nixos-rebuild build && \
+#sudo ls -l result && \
+#sudo nix store diff-closures /run/current-system ./result | awk '/[0-9] →|→ [0-9]/ && !/nixos/' || echo
+sudo nix store diff-closures /run/current-system ./result
 
 # https://discourse.nixos.org/t/how-to-get-this-pending-updates-notification-in-gnome/16344

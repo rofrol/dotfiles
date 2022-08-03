26652ea706e8fc7f30d90a68ad84d89e2896eb52
Roman Frołow <rofrol@gmail.com>
2022-08-04 01:19:18 +0200

    emacs: custom problem

--- .config/emacs/lisp/rf-custom.el
+++ .config/emacs/lisp/rf-custom.el
@@ -2,6 +2,8 @@
 
 ;; https://stackoverflow.com/questions/5052088/what-is-custom-set-variables-and-faces-in-my-emacs/5058752#5058752
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file)
+
+;; disabling as it causes problems: Symbol's function definition is void: custom-set-icon
+;; (load custom-file)
 
 (provide 'rf-custom)

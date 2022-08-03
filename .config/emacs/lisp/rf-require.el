64081ead12dfe42c11e7303c75e09b125a35c4af
Roman Frołow <rofrol@gmail.com>
2022-08-03 22:01:08 +0200

    rc/ to rf/

--- .config/emacs/lisp/rf-require.el
+++ .config/emacs/lisp/rf-require.el
@@ -1,6 +1,6 @@
 ;;; -*- lexical-binding: t; -*-
 
-;; https://github.com/rexim/dotfiles/blob/a529f79ffe3bac19fe1ce842c3296ad792757df7/.emacs.rc/rc.el#L1
+;; https://github.com/rexim/dotfiles/blob/a529f79ffe3bac19fe1ce842c3296ad792757df7/.emacs.rf/rc.el#L1
 
 ;; without below error: Symbol's value as variable is void: "package-archives"
 ;; https://www.reddit.com/r/emacs/comments/e990jg/comment/fah8qer/
@@ -11,20 +11,20 @@
 ;; (add-to-list 'package-archives
 ;;              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
-(defvar rc/package-contents-refreshed nil)
+(defvar rf/package-contents-refreshed nil)
 
-(defun rc/package-refresh-contents-once ()
-  (when (not rc/package-contents-refreshed)
-    (setq rc/package-contents-refreshed t)
+(defun rf/package-refresh-contents-once ()
+  (when (not rf/package-contents-refreshed)
+    (setq rf/package-contents-refreshed t)
     (package-refresh-contents)))
 
-(defun rc/require-one-package (package)
+(defun rf/require-one-package (package)
   (when (not (package-installed-p package))
-    (rc/package-refresh-contents-once)
+    (rf/package-refresh-contents-once)
     (package-install package)))
 
-(defun rc/require (&rest packages)
+(defun rf/require (&rest packages)
   (dolist (package packages)
-    (rc/require-one-package package)))
+    (rf/require-one-package package)))
 
 (provide 'rf-require)

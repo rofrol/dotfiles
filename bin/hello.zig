970d1f1dc81834c2cb66391be0770a17a301b077
Roman Frołow <rofrol@gmail.com>
2023-02-21 13:09:06 +0100

    gh cli: ghc, remove bin/hello.zig - moved to zig-scripts

--- bin/hello.zig
+++
@@ -1,5 +0,0 @@
-//usr/bin/env zig run --enable-cache "$0" -- "$@" | tail -n +2; exit;
-pub fn main() void {     
-    @import("std").debug.print("hello there\n",.{}); 
-}
-// https://www.reddit.com/r/Zig/comments/107wh7a/running_a_zig_file_like_a_script/

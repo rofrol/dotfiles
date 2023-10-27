d1f335b9cfd894aaaced697033dc99ad5f206084
Roman Frołow <rofrol@gmail.com>
2023-10-27 17:26:49 +0200

    bin/subtitles_encoding_and_line_ending.sh

---
+++ bin/subtitles_encoding_and_line_ending.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+FILE="$1"
+
+if [ ! -f "$FILE" ]; then
+	echo "Provide file name"
+	exit 1
+fi
+
+# EXT="${FILE##*.}"
+EXT=$(echo "$FILE" | sed 's/^.*\.//')
+NAME="${FILE%.*}"
+OUT="${NAME}.converted.${EXT}"
+# OUT="${NAME}.whatsapp.mp4"
+echo "OUT=${OUT}"
+
+iconv -f CP1250 -t UTF-8 "$FILE" >"$OUT"
+
+# - `Automator > Quick Action`
+#   - `Workflow receives current: files or folders`
+#   - `in: Finder.app`
+#   - on the left search for `Run Shell Script` and drag-and-drop it to the right.
+#   - `Pass input: as arguments`
+#
+# The conent will be:
+#
+# ```bash
+# . $HOME/.zprofile
+# file_name.sh "$@"
+# ```
+#
+# chmod +x file_name.sh
+
+# https://stackoverflow.com/questions/64860/best-way-to-convert-text-files-between-character-sets

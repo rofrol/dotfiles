4ed26cdea562821bdd7ab3bfe9a2e44dc497e5c5
Roman Frołow <rofrol@gmail.com>
2024-03-20 20:48:04 +0100

    iosevka: zed mono nerd font

--- iosevka/reinstall.sh
+++ iosevka/reinstall.sh
@@ -1,6 +1,6 @@
-cd ~/personal_projects/vendor/Iosevka && \
-npm run build -- ttf::IosevkaCustom && \
-cd ~/personal_projects/vendor/FontPatcher && \
-fontforge -script font-patcher --complete ~/personal_projects/vendor/Iosevka/dist/IosevkaCustom/TTF/IosevkaCustom-Regular.ttf && \
-cp IosevkaCustomNerdFont-Regular.ttf ~/iosevka/ && \
-open ~/iosevka/IosevkaCustomNerdFont-Regular.ttf
+cd ~/personal_projects/vendor/Iosevka &&
+	npm run build -- ttf::IosevkaCustom &&
+	cd ~/personal_projects/vendor/FontPatcher &&
+	fontforge -script font-patcher --complete ~/personal_projects/vendor/Iosevka/dist/IosevkaCustom/TTF/IosevkaCustom-Regular.ttf &&
+	cp IosevkaCustomNerdFont-Regular.ttf ~/iosevka/ &&
+	open ~/iosevka/IosevkaCustomNerdFont-Regular.ttf

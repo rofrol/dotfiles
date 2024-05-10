7240213d291a4b09428f7970aceceee96baf9c32
Roman Frołow <rofrol@gmail.com>
2024-05-10 04:52:21 +0200

    bin/video_for_whatsup_30fps.sh: some changes

--- bin/video_for_whatsup_30fps.sh
+++ bin/video_for_whatsup_30fps.sh
@@ -10,9 +10,9 @@ fi
 EXT="${FILE##*.}"
 NAME="${FILE%.*}"
 #OUT="${NAME}.whatsapp.${EXT}"
-OUT="${NAME}.whatsapp.mp4"
+OUT="${NAME}.whatsapp.30fps.mp4"
 echo "OUT=${OUT}"
 
-ffmpeg -i "$FILE" -c:v libx264 -profile:v baseline -level 3.0 -pix_fmt yuv420p -filter:v fps=30 "$OUT"
+ffmpeg -i "$FILE" -c:v libx265 -an -x265-params crf=25 "$OUT"
 
 # https://stackoverflow.com/questions/39887869/ffmpeg-whatsapp-video-format-not-supported/45882902#45882902

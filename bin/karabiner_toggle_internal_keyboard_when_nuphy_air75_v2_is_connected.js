0e41c9be9fc80b9f477ed240c15ef6b25ada9d87
Roman Frołow <rofrol@gmail.com>
2024-06-20 01:34:04 +0200

    karabiner: improve script to toggle internal keyboard

--- bin/karabiner_toggle_internal_keyboard_when_nuphy_air75_v2_is_connected.js
+++ bin/karabiner_toggle_internal_keyboard_when_nuphy_air75_v2_is_connected.js
@@ -9,11 +9,15 @@
 // 2. Add shortcut
 // System Settings > Keyboard > Keyboard Shortcuts > Services > General
 // Search for karabiner_toggle_internal_keyboard_when_nuphy_air75_v2_is_connected.js
-// On the right side click and set shortcut ctrl+shift+f12
+// On the right side click and set shortcut cmd+shift+0
+// Also I have created macro cmd+shift+0 and mapped it to screenshot key
 // https://apple.stackexchange.com/questions/24063/create-global-shortcut-to-run-command-line-applications/40887#40887
 
 // https://stackoverflow.com/questions/10685998/how-to-update-a-value-in-a-json-file-and-save-it-through-node-js/28175137#28175137
+
 import { writeFile } from "fs";
+import { exec } from "child_process";
+
 const debug = false;
 // https://stackoverflow.com/questions/21077670/expanding-resolving-in-node-js/54183941#54183941
 const fileName = "~/.config/karabiner/karabiner.json".replace(
@@ -33,9 +37,28 @@ if (profileIndex !== -1) {
   if (deviceIndex !== -1) {
     const device = file.profiles[profileIndex].devices[deviceIndex];
     if (debug) console.log(device);
-    console.log(device.disable_built_in_keyboard_if_exists);
     device.disable_built_in_keyboard_if_exists =
       !device.disable_built_in_keyboard_if_exists;
+    console.log(
+      "disable_built_in_keyboard_if_exists",
+      device.disable_built_in_keyboard_if_exists,
+    );
+
+    // https://gist.github.com/LukaszWiktor/80d2423072a7b88ec14d7688ff9b7433
+    exec(
+      `osascript -e 'display notification "disable_built_in_keyboard_if_exists: ${device.disable_built_in_keyboard_if_exists}"'`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+      },
+    );
   }
 }
 

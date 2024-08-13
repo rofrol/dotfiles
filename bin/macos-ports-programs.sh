8e860be1241764847e98b962e1aca36805ba5678
Roman Frołow <rofrol@gmail.com>
2024-08-13 12:27:17 +0200

    macos-ports-programs: use netstat

--- bin/macos-ports-programs.sh
+++ bin/macos-ports-programs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # https://x.com/NurmiWilliam/status/1823228630664634695
-sudo lsof -iTCP -sTCP:LISTEN -n -P | awk 'NR>1 {print $9, $1, $2}' | sed 's/.*://' | sort -u | while read port process pid; do echo "Port $port: $(ps -p $pid -o command= | sed 's/^-//') (PID: $pid)"; done | sort -n
+# sudo lsof -iTCP -sTCP:LISTEN -n -P | awk 'NR>1 {print $9, $1, $2}' | sed 's/.*://' | sort -u | while read port process pid; do echo "Port $port: $(ps -p $pid -o command= | sed 's/^-//') (PID: $pid)"; done | sort -n
+# https://apple.stackexchange.com/questions/117644/how-can-i-list-my-open-network-ports-with-netstat/408832#408832
+netstat -Watnlv | grep LISTEN | column -t -s "|"

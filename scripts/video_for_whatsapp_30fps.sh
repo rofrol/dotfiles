775c045630a0bbaf91e7a2de04f3d6555894dbcb
Roman Frołow <rofrol@gmail.com>
2026-03-05 14:07:14 +0100

    macOS Automator and zsh: env and homebrew initialization needs to be in .zshenv

--- scripts/video_for_whatsapp_30fps.sh
+++ scripts/video_for_whatsapp_30fps.sh
@@ -13,7 +13,11 @@ NAME="${FILE%.*}"
 OUT="${NAME}.whatsapp.30fps.mp4"
 echo "OUT=${OUT}"
 
-ffmpeg -i "$FILE" \
+# for ffmpeg to display in Automator output only errors from processing, add `-loglevel error -v quiet -stats`
+# https://www.reddit.com/r/bash/comments/18i8zpn/comment/kdbs4a8/
+# https://stackoverflow.com/questions/35169650/differentiate-between-error-and-standard-terminal-log-with-ffmpeg-nodejs/35215447#35215447
+ffmpeg -loglevel error -v quiet -stats \
+  -i "$FILE" \
   -vf "fps=30,scale=1280:-2" \
   -c:v libx264 -profile:v high -preset veryfast -crf 23 \
   -c:a aac -b:a 128k \
@@ -24,3 +28,26 @@ ffmpeg -i "$FILE" \
 # https://chatgpt.com/c/6803e780-0a34-800b-8d89-03ba249b174d
 # https://stackoverflow.com/questions/39887869/ffmpeg-whatsapp-video-format-not-supported/45882902#45882902
 # https://chatgpt.com/c/6803e555-8cb0-800b-83c8-4b74d624a9e0
+
+# - `Automator > Quick Action`
+#   - `Workflow receives current: files or folders`
+#   - `in: Finder.app`
+#   - on the left search for `Run Shell Script` and drag-and-drop it to the right.
+#   - `Pass input: as arguments`
+#   - `Cmd+s` to save it. It will be saved in `~/Library/Services`.
+#
+# The conent will be:
+#
+# ```bash
+#. $HOME/.profile
+#. $HOME/.zshrc
+# file_name.sh "$@"
+# ```
+#
+# chmod +x file_name.sh
+
+# Automator runs scripts in non-login and non-interactive mode. So env variable should be put in .zshenv
+# for homebrew add this to .zshenv:
+#if [[ -x /opt/homebrew/bin/brew ]]; then
+#    eval "$(/opt/homebrew/bin/brew shellenv)"
+#fi
